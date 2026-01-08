0043c9f012d97fc3bf6c8d69034ec4a45a3f2429
jiwon <panda981018@gmail.com>
2026-01-08 21:08:14 +0900

    Fix: set default timezone at Asia/Seoul

--- src/main/java/com/lifemanager/life_manager/LifeManagerApplication.java
+++ src/main/java/com/lifemanager/life_manager/LifeManagerApplication.java
@@ -1,11 +1,22 @@
 package com.lifemanager.life_manager;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class LifeManagerApplication {
 
+	@PostConstruct
+	public void started() {
+		// [핵심] 타임존을 한국 시간으로 강제 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재 시간: " + new java.util.Date());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LifeManagerApplication.class, args);
 	}
