e5fd1d47e9b5e88d0b8876e6534756eb6f0c257a
jiwon <panda981018@gmail.com>
2026-01-09 09:06:22 +0900

    Add EnableSpringDataWebSupport annotation (change pagination)

--- src/main/java/com/lifemanager/life_manager/LifeManagerApplication.java
+++ src/main/java/com/lifemanager/life_manager/LifeManagerApplication.java
@@ -4,10 +4,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import java.util.TimeZone;
 
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class LifeManagerApplication {
 
